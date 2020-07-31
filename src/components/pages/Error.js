78c35e503e927b2ad6133743e4552433e61c11b3
Mojgan360 <mojgan360@gmail.com>
2020-07-31 23:52:45 +0200

    Completed error page

--- src/components/pages/Error.js
+++ src/components/pages/Error.js
@@ -4,7 +4,7 @@ import Banner from '../layout/Banner'
 import { Link } from 'react-router-dom'
 const Error = () => {
   return (
-    <Hero>
+    <Hero max='true'>
       <Banner title='404' subtitle='page not found'>
         <Link to='/' className='btn-primary'>
           return home
